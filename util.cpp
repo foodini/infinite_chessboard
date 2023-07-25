aa2fd54ecdb4619e21d4abeecb3dc99fa6577200
Ron Barry <foodini@gmail.com>
2023-07-24 17:01:24 -0700

    Add min/max values to types. Add print_s128.

--- util.cpp
+++ util.cpp
@@ -2,6 +2,27 @@
 
 #include "util.h"
 
+u8 u8_min = 0x00;
+u8 u8_max = 0xff;
+s8 s8_min = -128;
+s8 s8_max = 127;
+u16 u16_min = 0x0000;
+u16 u16_max = 0xffff;
+s16 s16_min = 0x8000;
+s16 s16_max = 0x7fff;
+u32 u32_min = 0x00000000;
+u32 u32_max = 0xffffffff;
+s32 s32_min = 0x80000000;
+s32 s32_max = 0x7fffffff;
+u64 u64_min = 0x0000000000000000UL;
+u64 u64_max = 0xffffffffffffffffUL;
+s64 s64_min = 0x8000000000000000UL;
+s64 s64_max = 0x7fffffffffffffffUL;
+u128 u128_min = 0;
+u128 u128_max = ~((s128)1);
+s128 s128_min = ((s128)1) << 127;
+s128 s128_max = s128_min - 1;
+
 void print_u128(u128 u, char end) {
   char buf[80];
   char * cur = buf + 79;
@@ -12,6 +33,14 @@ void print_u128(u128 u, char end) {
     u /= 10;
   } while(u);
   printf("%s", cur + 1);
+}
+
+void print_s128(s128 s, char end) {
+  if(s<0) {
+    printf("-");
+    s = -s;
+  }
+  print_u128(s, end);
 }
 
 void print_x128(u128 u, char end) {
--- util.h
+++ util.h
@@ -1,17 +1,42 @@
+#ifndef _UTIL_H
+#define _UTIL_H
 #include <sys/time.h>
 
 typedef unsigned char u8;
+extern u8 u8_min;
+extern u8 u8_max;
 typedef char s8;
+extern s8 s8_min;
+extern s8 s8_max;
 typedef unsigned short u16;
+extern u16 u16_min;
+extern u16 u16_max;
 typedef short s16;
+extern s16 s16_min;
+extern s16 s16_max;
 typedef unsigned int u32;
+extern u32 u32_min;
+extern u32 u32_max;
 typedef int s32;
+extern s32 s32_min;
+extern s32 s32_max;
 typedef unsigned long u64;
+extern u64 u64_min;
+extern u64 u64_max;
 typedef long s64;
+extern s64 s64_min;
+extern s64 s64_max;
 typedef __uint128_t u128;
+extern u128 u128_min;
+extern u128 u128_max;
 typedef __int128_t s128;
+extern s128 s128_min;
+extern s128 s128_max;
 
 void print_u128(u128 u, char end='\0');
 void print_x128(u128 u, char end='\0');
+void print_s128(s128 u, char end='\0');
 double now();
 bool progress_timer();
+
+#endif // __UTIL_H
